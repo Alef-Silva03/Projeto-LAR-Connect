fffa64d9b3893c768d6c21204bfb9bfddceabe8c
Paty Stampa <patricia.stampa@tjrj.jus.br>
2026-02-22 09:17:52 -0300

    Add files via upload

--- src/main/resources/Front-end - Angular/src/app/pages/login/login.ts
+++ src/main/resources/Front-end - Angular/src/app/pages/login/login.ts
@@ -1,19 +1,39 @@
+import { AuthService } from './../../services/auth';  
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Adicione nos imports
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class Login {
-  constructor(private router: Router) {}
+  
+  dadosLogin = { login: '', senha: '' };
 
-  // Futura lógica de autenticação com seu backend Spring Boot
-  fazerLogin() {
-    console.log('Tentando logar...');
-    // Após sucesso: this.router.navigate(['/']);
+constructor(private authService: AuthService, private router: Router) {}
+
+fazerLogin() {
+  this.authService.login(this.dadosLogin).subscribe({
+  next: (usuarioLogado) => {
+    alert(`Bem-vindo, ${usuarioLogado.nome}!`);
+    localStorage.setItem('usuario', JSON.stringify(usuarioLogado));
+
+    // Redirecionamento baseado no perfil que vem do banco
+    if (usuarioLogado.perfil === 'SINDICO') {
+      this.router.navigate(['/dashboard-sindico']);
+    } else if (usuarioLogado.perfil === 'FUNCIONARIO') {
+      this.router.navigate(['/dashboard-funcionario']);
+    } else {
+      this.router.navigate(['/home-morador']);
+    }
+  },
+  error: (err) => {
+    alert('Email ou senha incorretos.');
   }
+});
+}     
 }
