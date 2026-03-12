43926bd2e46c8b7078077f2b4505d3a8c991af8f
Pedro Sena <psena9898@gmail.com>
2026-03-12 20:22:23 -0300

    Separação de Back-end e Front-end

--- src/main/java/com/projeto/larconnect/dto/ChatRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.larconnect.dto;
-
-public class ChatRequestDTO {
-    private String texto;
-
-    // getters e setters (ou use Lombok)
-    public String getTexto() { return texto; }
-    public void setTexto(String texto) { this.texto = texto; }
-}
