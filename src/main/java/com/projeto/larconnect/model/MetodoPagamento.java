09ec7a38d6cdd5186bee917b438918f77b61ec51
Pedro Sena <psena9898@gmail.com>
2026-03-12 20:14:17 -0300

    a

--- src/main/java/com/projeto/larconnect/model/MetodoPagamento.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.larconnect.model;
-
-public enum MetodoPagamento {
-    CARTAO_CREDITO,
-    CARTAO_DEBITO,
-    PIX,
-    BOLETO,
-    TRANSFERENCIA
-}
--- src/main/java/com/projeto/larconnect/model/StatusPagamento.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.larconnect.model;
-
-public enum StatusPagamento {
-    PENDENTE,
-    PROCESSANDO,
-    APROVADO,
-    RECUSADO,
-    ESTORNADO
-}
