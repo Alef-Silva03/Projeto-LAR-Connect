0a33377346fd602605497c7f2db519d641c4e3be
Paty Stampa <patricia.stampa@tjrj.jus.br>
2026-03-16 20:04:15 -0300

    Rename test suite from GestaoEncomendas to Auth

--- Front-end/src/app/services/gestao-encomendas.spec.ts
+++ Front-end/src/app/services/gestao-encomendas.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GestaoEncomendas } from './gestao-encomendas';
+import { Auth } from './auth';
 
-describe('GestaoEncomendas', () => {
-  let service: GestaoEncomendas;
+describe('Auth', () => {
+  let service: Auth;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(GestaoEncomendas);
+    service = TestBed.inject(Auth);
   });
 
   it('should be created', () => {
